ea6caa7298179ddfa5ade885a310a927a9700c9e
Thomas <developer@thomaswelzel.de>
2020-08-28 23:41:24 +0200

    start express project with gphoto2

--- app.js
+++ app.js
@@ -1,12 +1,60 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+var fs = require('fs');
+var gphoto2 = require('gphoto2');
+var GPhoto = new gphoto2.GPhoto2();
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+var indexRouter = require('./routes/index');
 
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+GPhoto.setLogLevel(1);
+GPhoto.on('log', function (level, domain, message) {
+  console.log(domain, message);
+});
+ 
+// List cameras / assign list item to variable to use below options
+GPhoto.list(function (list) {
+  if (list.length === 0) return;
+  var camera = list[0];
+  console.log('Found', camera.model);
+
+  camera.getConfig(function (er, settings) {
+    console.log(settings);
+  });
+
+  
+});
+module.exports = app;
